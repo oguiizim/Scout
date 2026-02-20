0875ada000d3fef9b1876159670d0e28f3d51ac6
Guilherme <guilhermecordeiroyt@gmail.com>
2026-02-19 23:52:43 -0300

    try3

--- server/src/main/java/com/guizim/server/configs/SecurityConfig.java
+++ server/src/main/java/com/guizim/server/configs/SecurityConfig.java
@@ -90,7 +90,7 @@ public class SecurityConfig {
                 CorsConfiguration configuration = new CorsConfiguration();
 
                 configuration.setAllowedOrigins(Arrays.asList(
-                                "http://localhost:5173", "https://scout-opal-nu.vercel.app"));
+                                "http://localhost:5173", "https://scout-two-ochre.vercel.app"));
 
                 configuration.setAllowedMethods(Arrays.asList(
                                 "GET", "POST", "PUT", "DELETE", "OPTIONS"));
