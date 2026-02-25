785c7cd6fef7997a07a350e53fab2e42807ad7f2
Guilherme <guilhermecordeiroyt@gmail.com>
2026-02-25 09:52:50 -0300

    .

--- server/src/main/java/com/guizim/server/configs/SecurityConfig.java
+++ server/src/main/java/com/guizim/server/configs/SecurityConfig.java
@@ -47,7 +47,8 @@ public class SecurityConfig {
         };
 
         private static final String[] PUBLIC_MATCHERS_DELETE = {
-                        "/user/{id}"
+                        "/user/{id}",
+                        "/team/{team}"
         };
 
         @Bean
